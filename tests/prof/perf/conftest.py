8e4a1ee220877dd1c69b0270632c023147e74b7c
Janek Nouvertné <provinzkraut@posteo.de>
2026-06-14 16:43:11 +0200

    set up codspeed

--- tests/prof/perf/conftest.py
+++ tests/prof/perf/conftest.py
@@ -3,6 +3,15 @@ import pytest
 from benchmarks.generate_data import make_filesystem_data
 
 
+# codspeed CI does not allow running a matrix, or submitting the same benchmarks twice
+# under the same name (e.g. one run under arm, one under amd). easiest way around this
+# was to use autoparametrisation to pretend the tests are different, and then select
+# them in CI via '-k <arch>'.
+@pytest.fixture(autouse=True, params=["amd", "arm"])
+def provide_fake_arch(request):
+    return request.param
+
+
 @pytest.fixture
 def bench(benchmark, bench_config):
     if not bench_config["calibrate"]:
