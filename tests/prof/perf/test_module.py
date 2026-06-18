206c13bbbef991f1cb44f72ea0ccf8debe5f3657
Janek Nouvertné <provinzkraut@posteo.de>
2026-06-18 21:10:30 +0200

    add more struct and module tests

--- tests/prof/perf/test_module.py
+++ tests/prof/perf/test_module.py
@@ -1,13 +1,27 @@
 import subprocess
 import sys
 
+import pytest
 
-def test_import_time(benchmark):
+
+@pytest.mark.parametrize(
+    "module_name",
+    [
+        "msgspec",
+        "msgspec.structs",
+        "msgspec.json",
+        "msgspec.msgpack",
+        "msgspec.inspect",
+        "msgspec.yaml",
+        "msgspec.toml",
+    ],
+)
+def test_import_time(benchmark, module_name):
     # also measures interpreter startup etc., but is the best we can do now, since we
     # don't have a fully isolated module, and some per-process global state, so
     # re-importing purely within the current interpreter doesn't get reliable timings
     def do_import():
-        subprocess.run([sys.executable, "-c", "import msgspec"], check=True)
+        subprocess.run([sys.executable, "-c", f"import {module_name}"], check=True)
 
     do_import()  # execute once to reduce possibility of a super cold FS cache
 
